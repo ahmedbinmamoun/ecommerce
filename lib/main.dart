8b2367acaaf2f0f4c8e8beec7aba996c00ee4328
Ahmed Mamoun <ahmedbinmamoun@gmail.com>
2025-09-02 14:07:39 +0300

    Initilize ecommerce app

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:ecommerce/core/utils/app_routes.dart';
+import 'package:ecommerce/core/utils/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.lightTheme,
+      );
+      },
+    );
+  }
+}
